21a225f8e6838c1f1f3b91ccf6a62785ceca6309
mcdrbogdanov <106861562+mcdrbogdanov@users.noreply.github.com>
2022-12-25 01:51:52 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import pymongo
 
 CLUSTER_CLIENT = pymongo.MongoClient('localhost', 27017) 
 
-BOT_TOKEN = ''
+BOT_TOKEN = '5781331229:AAFf_ZBvDE951OTMeJQF_t18Y-Yp4kgS4zA'
 
-BOT_NAME = ''
+BOT_NAME = '@Follbotgame_bot'
 
 BOT_DEVS = []
 
