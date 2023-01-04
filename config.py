91d4275ae37f57afc594676255b9e8f3e18abe41
mcdrbogdanov <106861562+mcdrbogdanov@users.noreply.github.com>
2023-01-04 06:01:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import pymongo
 
 CLUSTER_CLIENT = pymongo.MongoClient('localhost', 27017) 
 
-BOT_TOKEN = ''
+BOT_TOKEN = '5548529962:AAF7LB60g_l0FiH20jRGgV20Eb1orCMejZU'
 
-BOT_NAME = ''
+BOT_NAME = 'Gaming Bot'
 
-BOT_DEVS = []
+BOT_DEVS = [5348342250]
 
 TEMP_DIRECTION = 'temp'
 
